16cd1b7a06425ce119d5a80830f81c8c6f4b519c
NAVEENKUMAR M <118549015+Naveenk069@users.noreply.github.com>
2024-07-15 16:56:52 +0530

    Create Insertion_sort.c

---
+++ Insertion_sort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, temp, j;
+
+    for (i = 1; i < n; i++) {
+        temp = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;  
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
