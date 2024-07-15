23a956afe8aa3b540ae5b5e10fdd97510b28d1a3
NAVEENKUMAR M <118549015+Naveenk069@users.noreply.github.com>
2024-07-15 16:58:35 +0530

    Create Selection_sort.c

---
+++ Selection_sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_index;
+
+    for (i = 0; i < n - 1; i++) {
+        min_index = i;
+
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+
+        swap(&arr[min_index], &arr[i]);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
